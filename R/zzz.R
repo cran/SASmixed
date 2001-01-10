8206ea737294aae1ff602fb3791f234e20f399f7
Douglas Bates <bates@stat.wisc.edu>
2001-01-10 00:00:00 +0000

    version 0.1-3

--- R/zzz.R
+++ R/zzz.R
@@ -2,20 +2,20 @@
 ###
 ###    Sample analyses of data from SAS System for Mixed Models
 ###
-### Copyright 2000-2000  Douglas M. Bates <bates@stat.wisc.edu>
-###               
+### Copyright 2000-2001  Douglas M. Bates <bates@stat.wisc.edu>
+###
 ###
 ### This file is part of the SASmixed library for R and related languages.
 ### It is made available under the terms of the GNU General Public
 ### License, version 2, or at your option, any later version,
 ### incorporated herein by reference.
-### 
+###
 ### This program is distributed in the hope that it will be
 ### useful, but WITHOUT ANY WARRANTY; without even the implied
 ### warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 ### PURPOSE.  See the GNU General Public License for more
 ### details.
-### 
+###
 ### You should have received a copy of the GNU General Public
 ### License along with this program; if not, write to the Free
 ### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
